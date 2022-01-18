474e281e44f245c98a59d217b747cd4c8c5d1a2e
Tobie Allen <tallen@student.neumont.edu>
2022-01-18 15:35:07 -0700

    Movie example

---
+++ InClass/Controllers/MovieController.cs
@@ -0,0 +1,31 @@
+﻿using InClass.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InClass.Controllers
+{
+    public class MovieController : Controller
+    {
+        private static List<Movie> MovieList = new List<Movie>
+        {
+            new Movie("Iron Man", 2008, 5.0f),
+            new Movie("Blob", 2019, 5.0f),
+            new Movie("Man", 2003, 2.1f),
+            new Movie("Rando", 1998, 3.8f)
+        };
+
+        public IActionResult Index()
+        {
+            return View(MovieList);
+        }
+
+        public IActionResult DisplayMovie()
+        {
+            Movie m = new Movie("Iron Man", 2008, 5.0f); 
+            return View(m);
+        }
+    }
+}
---
+++ InClass/Models/Movie.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InClass.Models
+{
+    public class Movie
+    {
+        public string Title { get; set; } = "NO TITLE";
+        public int Year { get; set; } = 1850;
+        public float Rating { get; set; } = 0.0f;
+
+        public Movie() { }
+
+        public Movie(string title, int year, float rating)
+        {
+            this.Title = title;
+            this.Year = year;
+            this.Rating = rating; 
+        }
+
+        public override string ToString()
+        {
+            return $"{Title} - {Year} - {Rating} stars";
+        }
+    }
+}
