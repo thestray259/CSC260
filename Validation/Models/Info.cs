72afea89a962bcd1a7d78d2f4f0bff9c991ac9c1
Tobie Allen <tallen@student.neumont.edu>
2022-02-01 15:01:56 -0700

    Started Validation assignment

---
+++ Validation/Models/Info.cs
@@ -0,0 +1,25 @@
+﻿using Validation.Validators; 
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Validation.Models
+{
+    public class Info
+    {
+        [Required(ErrorMessage = "Name cannot be empty.")]
+        public string Name { get; set; }
+        [Required]
+        public int Age { get; set; }
+
+
+        public string StreetAddress { get; set; }
+        //[Required(ErrorMessage = "City cannot be empty.")]
+        public string City { get; set; }
+        //[Required(ErrorMessage = "State cannot be empty.")]
+        public string State { get; set; }
+        public int ZipCode { get; set; }
+    }
+}
---
+++ Validation/Validators/InfoAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Validation.Models; 
+
+namespace Validation.Validators
+{
+    public class InfoAttribute
+    {
+    }
+}
