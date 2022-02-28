d7ab8f72304bde33cb503e6eadd3765177644e7b
Tobie Allen <tallen@student.neumont.edu>
2022-02-28 15:47:47 -0700

    Added necessary views for final

--- SocialMediaSiteFinal/Controllers/HomeController.cs
+++ SocialMediaSiteFinal/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace SocialMediaSiteFinal.Controllers
             return View();
         }
 
+        public IActionResult AboutPage()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
