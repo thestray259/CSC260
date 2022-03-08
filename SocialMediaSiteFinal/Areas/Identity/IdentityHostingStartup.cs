ba6f38c6f46eb49b969befd3a85a32c0384ac328
Tobie Allen <tallen@student.neumont.edu>
2022-03-08 15:44:20 -0700

    Added Identity DB

---
+++ SocialMediaSiteFinal/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SocialMediaSiteFinal.Data;
+
+[assembly: HostingStartup(typeof(SocialMediaSiteFinal.Areas.Identity.IdentityHostingStartup))]
+namespace SocialMediaSiteFinal.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<IdentityContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("IdentityContextConnection")));
+
+                /*                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                                    .AddEntityFrameworkStores<IdentityContext>();*/
+
+                services.AddIdentity<IdentityUser, IdentityRole>
+                (options => options.SignIn.RequireConfirmedAccount = true)
+                .AddRoles<IdentityRole>()
+                .AddDefaultTokenProviders()
+                .AddDefaultUI()
+                .AddEntityFrameworkStores<IdentityContext>(); 
+
+                services.Configure<IdentityOptions>(options =>
+                {
+                    options.Password.RequireDigit = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequiredLength = 6;
+                    options.Password.RequiredUniqueChars = 0; 
+                });
+            });
+        }
+    }
+}
